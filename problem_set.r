119ea9e241742baa1aa5633d956e2e6ce53ef310
Alex Trickey <k.alex.trickey@gmail.com>
2019-05-15 07:55:14 +0000

    Add problem 3

--- problem_set.r
+++ problem_set.r
@@ -20,7 +20,6 @@ ad_data <- read.csv("adz/data/hourly_ad_category_data.csv",
 
 head(ad_data)
 tail(ad_data)
-# if in R Studio: View(ad_data)
 
 summary(ad_data)
 
@@ -28,10 +27,10 @@ summary(ad_data)
 # C. What problems do you see in the data? #
 ############################################
 
-#Let's check for missing values
+# Let's check for missing values
 apply(X = is.na(ad_data), MARGIN = 2, FUN = sum)
 
-#Why are there more missing values for rpi than rpc?
+# Why are there more missing values for rpi than rpc?
 sum(ad_data["imps"] < ad_data["clicks"])
 
 
@@ -68,23 +67,40 @@ ggplot(data = ad_data[ts >= "2019-05-13",],
 #########################################################
 library(forecast)
 
-ts1 <- ts(ad_data[ad_type == "dog_food", rpc], frequency = 24)
+# Dog food ad
+ts1 <- ts(ad_data[ad_type == "phone_service", rpc], frequency = 24)
 ts1 <- na.interp(ts1)
 
-######################################################
-# B. Create a Visualization of the TS using decomp() #
-######################################################
+# Cat toys ad
 
-decomp <- stl(ts1, s.window="periodic")
-plot(decomp)
 
-###############################################
-# C. Split the ts into train & test intervals #
-###############################################
+# Phone service ad
+
+
+
+#####################################################
+# B. Create Visualizations of the TS using decomp() #
+#####################################################
+
+# Dog food ad
+decomp1 <- stl(ts1, s.window="periodic")
+plot(decomp1)
+
+# Cat toys ad
+
+
+# Phone service ad
+
+
+
+###############################################################
+# C. Decide how to split the data into train & test intervals #
+###############################################################
+
 
 
 ############################################
-# C. Fit an ARIMA Model using auto.arima() #
+# D. Fit an ARIMA Model using auto.arima() #
 ############################################
 
 fit1 <- auto.arima(ts1[1:503])
@@ -92,7 +108,7 @@ accuracy(forecast(fit1, h=24), ts1[504:527])
 
 
 ######################################
-# D. Fit a TBATS Model using tbats() #
+# E. Fit a TBATS Model using tbats() #
 #    using a 24 hour seasonal period #
 ######################################
 
@@ -101,11 +117,100 @@ accuracy(forecast(fit2, h=24), ts1[504:527])
 
 
 ######################################
-# D. Fit a TBATS Model using tbats() #
+# F. Fit a TBATS Model using tbats() #
 #    using a 24 hour seasonal period #
 ######################################
 
 fit3 <- tbats(ts1[1:503], seasonal.periods = c(24, 7*24))
-accuracy(forecast(fit3, h=24), ts1[504:527])
+accuracy(forecast(fit3, h=10), ts1[504:527])
 
+
+#################
+# G. Questions? #
+#################
+
+# Based on the results above which model would you choose?
+
+
+# Our accuracy measures in the test sets here are likely to be better
+# than what we will observe in production. Where did we go wrong and
+# how can we correct the accuracy estimates?
+
+
+###########################################
+# Plot the forcast for you selected model #
+###########################################
+
+
+
+
+
+#############
+# Problem 3 #
+#############
+
+############################
+# A. Day1 Model Comparison #
+############################
+
+# Read in and check the data
+day1 <- fread("adz/data/day1.csv")
+head(day1)
+
+# Create confidence intervals for the RPS estimates
+stats <- day1[, .(m = mean(rps), sd = sd(rps), .N,
+                  moe = 1.96*sd(rps)/sqrt(.N)), by = baseline]
+stats[, `:=`(lower = m - moe, upper = m + moe)]
+stats
+
+# Is it safe to send more traffic to the new algo?
+
+
+
+############################
+# B. Day2 Model Comparison #
+############################
+
+# Repeat the analysis for the second day
+day2 <- fread("adz/data/day2.csv")
+stats <- day2[, .(m = mean(rps), sd = sd(rps), .N,
+                  moe = 1.96*sd(rps)/sqrt(.N)), by = baseline]
+stats[, `:=`(lower = m - moe, upper = m + moe)]
+stats
+
+# Should we increase the traffic % again?
+
+
+
+############################
+# C. Day3 Model Comparison #
+############################
+
+# Repeat the analysis for the third day
+
+# What is the lift our algo provides over baseline?
+
+
+#########################
+# D. Combine Comparison #
+#########################
+
+# Great things look good! Let's combine the three days, so we can report
+# back to the team...
+
+combined <- rbind(day1, day2, day3)
+stats <- combined[, .(m = mean(rps), sd = sd(rps), .N,
+                  moe = 1.96*sd(rps)/sqrt(.N)), by = baseline]
+stats[, `:=`(lower = m - moe, upper = m + moe)]
+stats
+
+#What happened? Why?
+
+# Visualize RPC (just show one day)
+library(ggplot2)
+combined[, date := as.Date(date)]
+combined[, baseline := as.factor(baseline)]
+ggplot(data = combined[, .(mean_rps = mean(rps), .N), by = .(baseline, date)],
+       aes(baseline, y = mean_rps, x = date, col = baseline, size = N)) +
+  geom_point()
 
